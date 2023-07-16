4731c3fb182e23167e7c3b0d7e15777cfed533c3
sunnad99 <sunnad99@gmail.com>
2023-07-16 23:32:15 +0500

    feat: Wrapped the 8ball Image widget with a TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,13 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset("images/ball1.png"),
+      child: TextButton(
+          onPressed: () {
+            print("I got clicked");
+          },
+          child: Image.asset(
+            "images/ball1.png",
+          )),
     );
   }
 }
