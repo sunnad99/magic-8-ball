8d8cb6790e7160b521aa9c016b21fe0c2b00828a
sunnad99 <sunnad99@gmail.com>
2023-07-16 23:23:55 +0500

    feat: Added the first scaffold with an AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       const MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Container(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
